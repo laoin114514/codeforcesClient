e902e871a39c60c62d4e0266d44c14a5c46f5967
laoin <2908451607@qq.com>
2026-06-01 22:27:49 +0800

    feat: 添加CFError错误类型与分类错误码

---
+++ errors.go
@@ -0,0 +1,32 @@
+package codeforcessdk
+
+import "fmt"
+
+type ErrorCode int
+
+const (
+	ErrNetwork     ErrorCode = iota
+	ErrAPI
+	ErrRateLimit
+	ErrAuth
+	ErrInvalidParam
+)
+
+type CFError struct {
+	Code    ErrorCode
+	Message string
+	Cause   error
+}
+
+func (e *CFError) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("[%d] %s: %v", e.Code, e.Message, e.Cause)
+	}
+	return fmt.Sprintf("[%d] %s", e.Code, e.Message)
+}
+
+func (e *CFError) Unwrap() error {
+	return e.Cause
+}
+
+var _ error = (*CFError)(nil)
